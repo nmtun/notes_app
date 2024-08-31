e3228d9bb05bf8bc132610d37341be52d86be103
nmtun <tungnlpb@gmail.com>
2024-08-31 22:18:06 +0700

    update

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -64,7 +64,7 @@ const SignUp = () => {
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleSignUp}>
-            <h4 className="text-center text-2xl mb-7">SignUp</h4>
+            <h4 className="text-center text-2xl mb-7">Sign Up</h4>
 
             <input 
               type="text" 
